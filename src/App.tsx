0aae4134862e213aa08201064f560fd9fb212f1c
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2023-05-26 14:47:18 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider } from 'react-router-dom';
 
-function App() {
+import './App.css';
+import { AsideNavigation } from './components/AsideNavigation';
+import { router } from './router';
+
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
-}
-
-export default App;
+};
